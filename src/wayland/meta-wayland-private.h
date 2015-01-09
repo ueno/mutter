cf8529af039f5a8964c829282695960e2d5a3bae
Carlos Garnacho <carlosg@gnome.org>
2015-01-09 17:23:17 +0100

    wayland: Add MetaWaylandTabletManager
    This struct keeps the server side information for the wl_tablet_manager
    global resource. It keeps the clients requesting this interface, and
    does keep track of the plugged tablet devices, so
    wl_tablet_manager.device_added is emitted on the expected clients.

--- src/wayland/meta-wayland-private.h
+++ src/wayland/meta-wayland-private.h
@@ -32,6 +32,7 @@
 #include "meta-wayland-versions.h"
 #include "meta-wayland-surface.h"
 #include "meta-wayland-seat.h"
+#include "meta-wayland-tablet-manager.h"
 
 typedef struct
 {
@@ -64,6 +65,7 @@ struct _MetaWaylandCompositor
   MetaXWaylandManager xwayland_manager;
 
   MetaWaylandSeat *seat;
+  MetaWaylandTabletManager *tablet_manager;
 };
 
 #endif /* META_WAYLAND_PRIVATE_H */
--- src/wayland/meta-wayland-types.h
+++ src/wayland/meta-wayland-types.h
@@ -33,6 +33,7 @@ typedef struct _MetaWaylandTouch MetaWaylandTouch;
 typedef struct _MetaWaylandDataSource MetaWaylandDataSource;
 typedef struct _MetaWaylandDataDevice MetaWaylandDataDevice;
 
+typedef struct _MetaWaylandTabletManager MetaWaylandTabletManager;
 typedef struct _MetaWaylandTablet MetaWaylandTablet;
 
 typedef struct _MetaWaylandBuffer MetaWaylandBuffer;
